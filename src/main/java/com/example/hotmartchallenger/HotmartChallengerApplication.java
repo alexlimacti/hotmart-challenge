655ca52af149b752ec5e82168b6889dd8f10a75d
Alexsandro de Lima Silva <alexlima_cti@hotmail.com>
2021-09-09 22:50:15 -0300

    criacao do projeto

---
+++ src/main/java/com/example/hotmartchallenger/HotmartChallengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotmartchallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmartChallengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotmartChallengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotmartchallenger/HotmartChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotmartchallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotmartChallengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
